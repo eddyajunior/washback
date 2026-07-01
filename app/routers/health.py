d757f40460ee0bd65c80ae692485c83c88aa3e25
Edson Amaral <eddyajunior@gmail.com>
2026-07-01 16:02:07 -0300

    refact:Melhora os logs do endpoint health

--- app/routers/health.py
+++ app/routers/health.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import text 
+from sqlalchemy.orm import Session
 
-from app.core.responses import success_response
+from app.infrastructure.database.database import get_db
+
+# from app.core.responses import success_response
 
 router = APIRouter(
     prefix="/health",
@@ -8,11 +12,20 @@ router = APIRouter(
 )
 
 @router.get("/")
-def healthcheck():
+def healthcheck(
+    db: Session = Depends(get_db)
+):
+    db.execute(text("SELECT 1"))
 
-    return success_response(
-        "API online",
-        {
-            "status": "ok"
-        }
-    )
+    return {
+        "status": "healthy",
+        "app": "WashBack",
+        "database": "connected"
+    }
+
+    # return success_response(
+    #     "API online",
+    #     {
+    #         "status": "ok"
+    #     }
+    # )
